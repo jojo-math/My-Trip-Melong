695a26dd0ad641c7c85e5e527d9fbeceb18af141
MELONG TSAYEM Joan <joantsayem@gmail.com>
2025-09-10 21:12:57 +0100

    Modification du header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
 import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{title:"Home"}}></Stack.Screen>
-    </Stack>
+      <Stack.Screen name="index" options={{title:"Découvrez mon merveilleux voyage", headerStyle:{backgroundColor:'rgba(172, 70, 66, 1)'}}}></Stack.Screen>
+    </Stack> 
     );
 }
+
+const styleHome = StyleSheet.create({
+  header:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+});
--- app/index.tsx
+++ app/index.tsx
@@ -18,16 +18,15 @@ export default function Index() {
     >
       <ScrollView snapToInterval={200} decelerationRate="fast" showsVerticalScrollIndicator={false}>
       <ScrollView horizontal snapToInterval={240} decelerationRate="fast" showsHorizontalScrollIndicator={false} contentContainerStyle={{alignItems:'center'}} style={{height:300, backgroundColor:'rgba(61, 204, 25, 1)', opacity:0.75}}>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
-        <Image source={imgSlide1} style={customizeImage.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
+        <Image source={imgSlide1} style={customizer.sizeImage}></Image>
       </ScrollView>
-      
         <Text style={{fontSize:20, margin:20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
         <Text style={{fontSize:20, margin:20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
         <Text style={{fontSize:20, margin:20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
@@ -46,14 +45,19 @@ export default function Index() {
   );
 }
 
-const customizeImage = StyleSheet.create({
+const customizer = StyleSheet.create({
   sizeImage:{
     height:200,
     width:400,
     borderRadius:20,
     marginLeft:40,
     borderColor:'gray',
-    borderWidth: 2
-  }
+    borderWidth: 2,
+    shadowOffset: {width: 0, height:6},
+    shadowColor: 'black',
+    shadowRadius: 16,
+    shadowOpacity:0.5
+  },
+  styleText:{}
 })
 
