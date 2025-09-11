7a617d586fbf4a2a0adc97d8d7542cd2cb6fba75
MELONG TSAYEM Joan <joantsayem@gmail.com>
2025-09-11 22:14:42 +0100

    Correction du lien de la page index

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { StyleSheet, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.size}>
-            <Link href={"/views/trip"} style={styles.links}>Cliquez pour découvrir mon expérience de voyage...</Link>
+            <Link href={"/views/trip"} style={styles.links}>Cliquez pour découvrir...</Link>
         </View>
     
   );
@@ -15,14 +15,15 @@ export default function Index() {
 const styles = StyleSheet.create({
     links:{
         flex: 1,
-        height: 60,
-        maxWidth: 600,
+        minHeight: 50,
+        maxWidth: 500,
         alignItems: "center",
         alignSelf:'center',
+        textAlign:'center',
         backgroundColor: "#2E7D32",
         elevation: 3,
         borderRadius: 12,
-        paddingHorizontal: 32,
+        paddingHorizontal: 24,
         paddingVertical:8,
         fontSize:20,
         color:'white',
